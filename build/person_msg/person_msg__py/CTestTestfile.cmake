532ada11a51823ffdcc1332252779615b70b1317
Setsu Ito <setsu.god@gmail.com>
2022-11-24 12:55:11 +0900

    Initial Commit

---
+++ build/person_msg/person_msg__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/setsu/ros2_ws/src/person_msg/build/person_msg/person_msg__py
+# Build directory: /home/setsu/ros2_ws/src/person_msg/build/person_msg/person_msg__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
